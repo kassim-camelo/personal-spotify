986fb7189b12aca0f2d703d2eb1d2b2bb1358bde
Thiago Camelo <thiago.la1707@gmail.com>
2024-05-08 13:12:51 -0300

    feat: Add token retrieval and display in Info component

---
+++ src/services/loginService.ts
@@ -0,0 +1,13 @@
+export async function getToken(){
+    const clientId = ''
+    const clientSecret = ''
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
+    })
+    const data = await response.json()
+    return data;
+}
